903354a4aeaedd04ad4ff84875466e97ecd77725
Phil Hanna <ph1204@gmail.com>
2023-05-23 16:00:37 -0400

    Added unit test for GetSelectedIndices

--- ncvoters/create_database_test.go
+++ ncvoters/create_database_test.go
@@ -1,6 +1,10 @@
 package ncvoters
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestCreateInsertSQL(t *testing.T) {
 	tests := []struct {
@@ -12,9 +16,35 @@ func TestCreateInsertSQL(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := CreateInsertSQL(tt.cols); got != tt.want {
-				t.Errorf("CreateInsertSQL() = %v, want %v", got, tt.want)
-			}
+			want := tt.want
+			have := CreateInsertSQL(tt.cols)
+			assert.Equal(t, want, have)
+		})
+	}
+}
+
+func TestGetSelectedIndices(t *testing.T) {
+	all := []string{
+		"Larry",
+		"Curly",
+		"Moe",
+	}
+	tests := []struct {
+		name   string
+		subset []string
+		want   []int
+	}{
+		{"Find all", all, []int{0, 1, 2}},
+		{"Empty list", []string{}, []int{}},
+		{"Just one", []string{"Larry"}, []int{0}},
+		{"Just two", []string{"Curly", "Moe"}, []int{1, 2}},
+		{"Bogus", []string{"Bogus"}, []int{}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			want := tt.want
+			have := GetSelectedIndices(all, tt.subset)
+			assert.Equal(t, want, have)
 		})
 	}
 }
