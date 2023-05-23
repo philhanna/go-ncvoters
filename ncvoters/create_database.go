4407df174790f0d94b8315df6319658ca35b2962
Phil Hanna <ph1204@gmail.com>
2023-05-23 15:50:50 -0400

    Added GetSelectedIndices

--- ncvoters/create_database.go
+++ ncvoters/create_database.go
@@ -1,16 +1,18 @@
 package ncvoters
 
 import (
+	"archive/zip"
 	"database/sql"
-	"encoding/csv"
+
+	// "encoding/csv"
 	"fmt"
-	"io"
+	// "io"
 	"log"
-	"os"
+	// "os"
 	"strings"
 )
 
-func CreateDatabase(csvFileName, dbFileName string, progressEvery int) error {
+func CreateDatabase(zipFileName, csvFileName, dbFileName string, progressEvery int) error {
 	log.Println("Creating database...")
 
 	// Open the database
@@ -45,83 +47,98 @@ func CreateDatabase(csvFileName, dbFileName string, progressEvery int) error {
 	}
 	defer stmt.Close()
 
-	csvFile, err := os.Open(csvFileName)
+	// Open the zip file
+	zipFile, err := zip.OpenReader(zipFileName)
 	if err != nil {
 		log.Println(err)
 		return err
 	}
-	defer csvFile.Close()
+	defer zipFile.Close()
 
-	reader := csv.NewReader(csvFile)
-	reader.FieldsPerRecord = -1 // Allow varying number of fields
-
-	columns, err := reader.Read()
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-
-	selectedIndices := make([]int, 0)
-	for i, col := range columns {
-		found := false
-		for _, element := range selectedCols {
-			if strings.Contains(element, col) {
-				found = true
-				break
-			}
-		}
-		if found {
-			selectedIndices = append(selectedIndices, i)
-		}
-	}
-
-	count := 0
-	for {
-		record, err := reader.Read()
-		if err == io.EOF {
-			log.Println(err)
+	// Find the CSV file in the zip archive
+	var csvFile *zip.File
+	for _, file := range zipFile.File {
+		if file.Name == csvFileName {
+			csvFile = file
 			break
-		} else if err != nil {
+		}
+	}
+
+	// If the CSV file is not found, exit with an error
+	if csvFile == nil {
+		err = fmt.Errorf("file %q not found in zip archive", csvFileName)
+		log.Println(err)
+		return err
+	}
+
+	// Open the CSV file inside the archive
+	csvReader, err := csvFile.Open()
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	defer csvReader.Close()
+	/*
+		// Read the CSV file into a byte buffer
+
+
+
+
+
+		csvFile, err := os.Open(csvFileName)
+		if err != nil {
 			log.Println(err)
 			return err
 		}
+		defer csvFile.Close()
 
-		values := make([]interface{}, len(selectedIndices))
-		for i, idx := range selectedIndices {
-			values[i] = record[idx]
-		}
+		reader := csv.NewReader(csvFile)
+		reader.FieldsPerRecord = -1 // Allow varying number of fields
 
-		_, err = stmt.Exec(values...)
+		columns, err := reader.Read()
 		if err != nil {
 			log.Println(err)
 			return err
 		}
 
-		count++
-		if count%progressEvery == 0 {
-			log.Printf("%d records inserted", count)
+		selectedIndices := GetSelectedIndices(columns, selectedCols)
+
+		count := 0
+		for {
+			record, err := reader.Read()
+			if err == io.EOF {
+				log.Println(err)
+				break
+			} else if err != nil {
+				log.Println(err)
+				return err
+			}
+
+			values := make([]interface{}, len(selectedIndices))
+			for i, idx := range selectedIndices {
+				values[i] = record[idx]
+			}
+
+			_, err = stmt.Exec(values...)
+			if err != nil {
+				log.Println(err)
+				return err
+			}
+
+			count++
+			if count%progressEvery == 0 {
+				log.Printf("%d records inserted", count)
+			}
 		}
-	}
 
-	err = tx.Commit()
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-
+		err = tx.Commit()
+		if err != nil {
+				log.Println(err)
+			return err
+		}
+	*/
 	log.Println("Database created successfully!")
 	return nil
-}
-
-// CreatePreparedStatement creates an SQL statement for inserting
-// records into the voters table.
-func CreatePreparedStatement(tx *sql.Tx) (*sql.Stmt, error) {
-	sqlString := CreateInsertSQL(selectedCols)
-	stmt, err := tx.Prepare(sqlString)
-	if err != nil {
-		return nil, err
-	}
-	return stmt, nil
 }
 
 // CreateInsertSQL creates an SQL string that can be used for inserting
@@ -140,4 +157,54 @@ func CreateInsertSQL(cols []string) string {
 	sqlString := fmt.Sprintf("INSERT INTO voters (%s) VALUES (%s)", colNames, qMarks)
 
 	return sqlString
+}
+
+// CreatePreparedStatement creates an SQL statement for inserting
+// records into the voters table.
+func CreatePreparedStatement(tx *sql.Tx) (*sql.Stmt, error) {
+	sqlString := CreateInsertSQL(selectedCols)
+	stmt, err := tx.Prepare(sqlString)
+	if err != nil {
+		return nil, err
+	}
+	return stmt, nil
+}
+
+// GetSelectedIndices returns the indices of selected columns from a
+// given list of columns.
+//
+// It takes two parameters: 'columns' - a slice of strings representing
+// all available columns, and 'selectedCols' - a slice of strings
+// representing the columns that are selected.
+//
+// The function iterates over the 'columns' slice, checks if each column
+// exists in the 'selectedCols' slice, and if found, appends the index
+// of the column to the 'selectedIndices' slice.  Finally, it returns
+// the 'selectedIndices' slice containing the indices of selected
+// columns.
+func GetSelectedIndices(columns, selectedCols []string) []int {
+
+	// Create an empty slice to store the selected column indices.
+	selectedIndices := make([]int, 0)
+
+	// Iterate over each column in the 'columns' slice.
+	for i, col := range columns {
+
+		// Initialize a flag variable to track if the column is found in 'selectedCols'.
+		found := false
+
+		// Iterate over each element in 'selectedCols'.
+		for _, element := range selectedCols {
+			if element == col {
+				found = true
+				break
+			}
+		}
+		if found {
+			selectedIndices = append(selectedIndices, i)
+		}
+	}
+
+	// Return the slice containing the indices of selected columns.
+	return selectedIndices
 }
