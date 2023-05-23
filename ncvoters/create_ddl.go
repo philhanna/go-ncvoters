24e8b272a18202d5388d8b10a806392f66dcaf61
Phil Hanna <ph1204@gmail.com>
2023-05-23 16:46:59 -0400

    Added comments

--- ncvoters/create_ddl.go
+++ ncvoters/create_ddl.go
@@ -5,9 +5,13 @@ import (
 	"strings"
 )
 
-func CreateDDL() string {	
+// This function creates a Data Definition Language (DDL) statement for
+// creating a table called `voters`.
+func CreateDDL() string {
 	sb := strings.Builder{}
+
 	sb.WriteString("CREATE TABLE IF NOT EXISTS voters (\n")
+
 	for i, colName := range selectedCols {
 		comma := ","
 		if i == len(selectedCols)-1 {
@@ -16,7 +20,10 @@ func CreateDDL() string {
 		part := fmt.Sprintf("  %-20s TEXT%s\n", colName, comma)
 		sb.WriteString(part)
 	}
+
 	sb.WriteString(")\n")
+
 	s := sb.String()
+
 	return s
 }
