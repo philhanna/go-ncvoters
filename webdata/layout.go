5e8327abeeb4f2da98b6fbafa70ae6c3e0a34ea2
Phil Hanna <ph1204@gmail.com>
2023-12-24 07:19:56 -0500

    Added reason codes parsing

--- webdata/layout.go
+++ webdata/layout.go
@@ -19,6 +19,7 @@ type Layout struct {
 	RaceCodes   map[string]string
 	EthnicCodes map[string]string
 	CountyCodes map[int]string
+	ReasonCodes map[string]string
 }
 
 // ---------------------------------------------------------------------
@@ -28,10 +29,11 @@ type Layout struct {
 const (
 	URL                = "https://s3.amazonaws.com/dl.ncsbe.gov/data/layout_ncvoter.txt"
 	TABLE_COLUMNS      = "columns"
-	TABLE_STATUS_CODES = "status_codes"
-	TABLE_RACE_CODES   = "race_codes"
-	TABLE_ETHNIC_CODES = "ethnic_codes"
 	TABLE_COUNTY_CODES = "county_codes"
+	TABLE_ETHNIC_CODES = "ethnic_codes"
+	TABLE_RACE_CODES   = "race_codes"
+	TABLE_REASON_CODES = "reason_codes"
+	TABLE_STATUS_CODES = "status_codes"
 )
 
 // ---------------------------------------------------------------------
@@ -46,6 +48,7 @@ func NewLayout() *Layout {
 	layout.RaceCodes = make(map[string]string)
 	layout.EthnicCodes = make(map[string]string)
 	layout.CountyCodes = make(map[int]string)
+	layout.ReasonCodes = make(map[string]string)
 	return layout
 }
 
@@ -99,6 +102,42 @@ readLoop:
 		}
 	}
 
+	// Add [undocumented] reason codes
+	fp.WriteString("/* ****************************************************************************")
+	fp.WriteString("Reason codes")
+	fp.WriteString("reason_cd  voter_status_reason_desc")
+	fp.WriteString("*******************************************************************************")
+	fp.WriteString("A1         UNVERIFIED")
+	fp.WriteString("A2         CONFIRMATION PENDING")
+	fp.WriteString("AA         ARMED FORCES")
+	fp.WriteString("AL         LEGACY DATA")
+	fp.WriteString("AN         UNVERIFIED NEW")
+	fp.WriteString("AP         VERIFICATION PENDING")
+	fp.WriteString("AV         VERIFIED")
+	fp.WriteString("DI         UNAVAILABLE ESSENTIAL INFORMATION")
+	fp.WriteString("DN         CONFIRMATION NOT RETURNED")
+	fp.WriteString("DU         VERIFICATION RETURNED UNDELIVERABLE")
+	fp.WriteString("IA         ADMINISTRATIVE")
+	fp.WriteString("IL         LEGACY - CONVERSION")
+	fp.WriteString("IN         CONFIRMATION NOT RETURNED")
+	fp.WriteString("IU         CONFIRMATION RETURNED UNDELIVERABLE")
+	fp.WriteString("R2         DUPLICATE")
+	fp.WriteString("RA         ADMINISTRATIVE")
+	fp.WriteString("RC         REMOVED DUE TO SUSTAINED CHALLENGE")
+	fp.WriteString("RD         DECEASED")
+	fp.WriteString("RF         FELONY CONVICTION")
+	fp.WriteString("RH         MOVED WITHIN STATE")
+	fp.WriteString("RL         MOVED FROM COUNTY")
+	fp.WriteString("RM         REMOVED AFTER 2 FED GENERAL ELECTIONS IN INACTIVE STATUS")
+	fp.WriteString("RP         REMOVED UNDER OLD PURGE LAW")
+	fp.WriteString("RQ         REQUEST FROM VOTER")
+	fp.WriteString("RR         FELONY SENTENCE COMPLETED")
+	fp.WriteString("RS         MOVED FROM STATE")
+	fp.WriteString("RT         TEMPORARY REGISTRANT")
+	fp.WriteString("SM         MILITARY")
+	fp.WriteString("SO         OVERSEAS CITIZEN")
+	fp.WriteString("**************************************************************************** */")
+
 	return filename, nil
 
 }
@@ -116,6 +155,7 @@ func (layout *Layout) GetMetadataDDL() (string, error) {
 	sb.WriteString(CreateRaceCodesDDL(layout.RaceCodes))
 	sb.WriteString(CreateEthnicCodesDDL(layout.EthnicCodes))
 	sb.WriteString(CreateCountyCodesDDL(layout.CountyCodes))
+	sb.WriteString(CreateReasonCodesDDL(layout.ReasonCodes))
 	ddl := sb.String()
 	return ddl, nil
 }
