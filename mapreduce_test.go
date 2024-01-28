7d9578998ea474cb9a68496d50fba6feb376863d
Phil Hanna <ph1204@gmail.com>
2024-01-28 17:19:31 -0500

    Added unit test for Map

--- mapreduce_test.go
+++ mapreduce_test.go
@@ -13,20 +13,9 @@ func TestMap(t *testing.T) {
 	}
 
 	inch := make(chan any)
+	inputs := []any{1, 2, 3, 4, 5}
 	ouch := Map(square, inch)
 
-	inputs := []any{1, 2, 3}
-	expectedOutputs := []any{1, 4, 9}
-
-	go func() {
-		defer close(ouch)
-		for _, output := range expectedOutputs {
-			ouch <- output
-		}
-	}()
-
-	result := Map(square, inch)
-	
 	go func() {
 		for _, input := range inputs {
 			inch <- input
@@ -34,13 +23,13 @@ func TestMap(t *testing.T) {
 		close(inch)
 	}()
 
-	for expected := range ouch {
-		actual, ok := <- result
+	for _, expected := range []any{1, 4, 9, 16, 25} {
+		actual, ok := <-ouch
 		assert.True(t, ok)
 		assert.Equal(t, expected, actual)
 	}
 
-	if _, ok := <-result; ok {
+	if _, ok := <-ouch; ok {
 		t.Fatal("Received more outputs than expected")
 	}
 }
