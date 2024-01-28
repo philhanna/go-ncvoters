a397df8a8f354a810af04fd9d6c5c22282cd10d8
Phil Hanna <ph1204@gmail.com>
2024-01-28 12:57:42 -0500

    Added mapreduce.go

---
+++ mapreduce.go
@@ -0,0 +1,21 @@
+package goncvoters
+
+func Map(f func(any) any, inch chan any) chan any {
+	ouch := make(chan any)
+	go func() {
+		defer close(ouch)
+		for input := range inch {
+			output := f(input)
+			ouch <- output
+		}
+	}()
+	return ouch
+}
+
+func Reduce(f func(x, y any) any, inch chan any, initial any) any {
+	accumulator := initial
+	for k := range inch {
+		accumulator = f(accumulator, k)
+	}
+	return accumulator
+}
